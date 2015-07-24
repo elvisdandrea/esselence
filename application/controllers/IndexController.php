5fd9cddd57a5c83cd9cae43fe29640440ea8fa77
Elvis D'Andrea <elvis@gravi.com.br>
2015-07-23 22:13:44 -0300

    Initial Frames

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -1,15 +1,27 @@
 <?php
 
+/**
+ * Class IndexController
+ *
+ * The Main Project starts here
+ *
+ * For everyone editing this, please check the rules:
+ *
+ * - Do not code PSR, it sucks
+ * - Brackets are on the same line of the method
+ * - If you don't sacrifice readability for brevity, you are a pussy
+ * - Real code has real small methods reusing functions as much as possible
+ * - This means that low level code must be on the core, not on your method
+ *
+ */
 class IndexController extends Zend_Controller_Action
 {
 
-    public function init()
-    {
+    public function init() {
         /* Initialize action controller here */
     }
 
-    public function indexAction()
-    {
+    public function indexAction() {
         // action body
     }
 
