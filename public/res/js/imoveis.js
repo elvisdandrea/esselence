43bf2e0c2c252c592c16e85a08e01bf413ce7d29
Elvis D'Andrea <elvis@gravi.com.br>
2015-08-03 23:49:19 -0300

    Search More with params

--- public/res/js/imoveis.js
+++ public/res/js/imoveis.js
@@ -1,7 +1,8 @@
 $(window).scroll(function() {
     if($(window).scrollTop() + $(window).height() == $(document).height()) {
+        var params = parseGetParams();
         $.ajax({
-            url : '/esselence/public/imoveis/listagem',
+            url : '/esselence/public/imoveis/listagem?' + location.search.substr(1),
             success : function(html){
                 $('#lista-imoveis').append(html);
             }
